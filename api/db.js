e318640e468354db6b2cec5c0434caab42fb9d20
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-11-06 21:09:33 -0500

    third refactor

---
+++ api/db.js
@@ -0,0 +1,26 @@
+let data = { 
+    products: {
+        one: {
+            img_url: "./assets/images/shoe.png", 
+            descript: "These shoes are made in the same factory as:", 
+            brand: "Nike", 
+            price: "$80"
+        }, 
+        three: {
+            img_url: "./assets/images/toothbrush.png",
+            descript: "This toothbrush is made in the same factory as:",
+            brand: "Quip",
+            price: "$35"
+        }, 
+        five: {
+            img_url: "./assets/images/lipstick.png",
+            descript: "This lipstick is made in the same factory as:",
+            brand: "La Mer",
+            price: "$20"
+        }
+
+    }, 
+    faq: [
+        "What is Italic?", "Italic is a marketplace that lets consumers shop from the world's best factories. No logos, no brands — factories make a lot more money, consumers get unbeatable value, and Italic handles everything in between."
+    ]
+}
---
+++ index.js
@@ -0,0 +1,86 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+
+    console.log('hi')
+
+    // build a panel
+    // function productText(section) {
+    //     let sect = document.getElementById(section)
+    //     console.log(sect)
+    //     let sectData = data["products"][section]
+    //     console.log(sectData)
+
+    //     let price = sect.getElementsByClassName('price')[0]
+    //     console.log(price)
+    //     price.innerText = sectData.price
+
+    //     let img = sect.getElementsByClassName('prod-img')[0]
+    //     img.src = sectData.img_url
+
+    //     let descript = sect.getElementsByClassName('descript')[0]
+    //     descript.innerText = sectData.descript
+
+    //     let brand = sect.getElementsByClassName('brand')[0]
+    //     brand.innerText = sectData.brand
+    // }
+
+    // productText('one')
+    // productText('three')
+    // productText('five')
+
+    function faq() {
+        let sect = document.getElementsByClassName('questions-container')[0]
+        console.log(data["faq"])
+
+        let counter = 0
+        while (counter < 4) {
+            let div = document.createElement('div')
+            div.className = 'faq-item-container'
+
+            let q = document.createElement('p')
+            q.className = "faq-sub-header"
+            q.innerText = data["faq"][0]
+
+            let a = document.createElement('p')
+            a.className = "faq-body-type"
+            a.innerText = data["faq"][1]
+
+            div.appendChild(q)
+            div.appendChild(a)
+            sect.appendChild(div)
+
+            counter++
+        }
+    }
+
+    faq()
+
+
+    //pin email form to navigation
+    // var controller = new ScrollMagic.Controller({
+    //     globalSceneOptions: {
+    //         triggerHook: 'onLeave'
+    //     }
+    // });
+
+    var pin = new ScrollMagic.Scene({
+        triggerElement: '#test',
+    }).setPin('#test', { pushFollowers: false });
+    pin.setClassToggle('#test', 'nav-class');
+    pin.addTo(controller);
+
+
+    //enter timeline 
+    var tl = new TimelineMax();
+    tl.to("#things", 1.5, { opacity: 1 });
+    tl.to("#prices", 1.5, { opacity: 1 });
+    tl.to("#brands", 1.5, { opacity: 1 });
+
+
+    //second page timeline
+    // var secondEnter = new ScrollMagic.Scene({
+    //     triggerElement: '#test',
+    // })
+    //     .setClassToggle('#second-scene', 'nav-class')
+    //     .addTo(controller)
+
+})
