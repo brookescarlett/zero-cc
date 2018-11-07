54d4d8c8e63811f54925bc3714076c0834a99b71
brookescarlett <brookeyalof2017@u.northwestern.edu>
2018-11-07 04:48:46 -0500

    styling updates

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="dist",n(n.s=0)}([function(e,t){document.addEventListener("DOMContentLoaded",function(e){console.log("hi"),function(){var e=document.getElementsByClassName("questions-container")[0];console.log(data.faq);for(var t=0;t<4;){var n=document.createElement("div");n.className="faq-item-container";var r=document.createElement("p");r.className="faq-sub-header",r.innerText=data.faq[0];var o=document.createElement("p");o.className="faq-body-type",o.innerText=data.faq[1],n.appendChild(r),n.appendChild(o),e.appendChild(n),t++}}();var t=new ScrollMagic.Scene({triggerElement:"#test"}).setPin("#test",{pushFollowers:!1});t.setClassToggle("#test","nav-class"),t.addTo(controller);var n=new TimelineMax;n.to("#things",1.5,{opacity:1}),n.to("#prices",1.5,{opacity:1}),n.to("#brands",1.5,{opacity:1})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "dist";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("document.addEventListener('DOMContentLoaded', function (e) {\n  function faq() {\n    var sect = document.getElementsByClassName('questions-container')[0];\n    console.log(data[\"faq\"]);\n    var counter = 0;\n\n    while (counter < 4) {\n      var div = document.createElement('div');\n      div.className = 'faq-item-container';\n      var q = document.createElement('p');\n      q.className = \"faq-sub-header\";\n      q.innerText = data[\"faq\"][0];\n      var a = document.createElement('p');\n      a.className = \"faq-body-type\";\n      a.innerText = data[\"faq\"][1];\n      div.appendChild(q);\n      div.appendChild(a);\n      sect.appendChild(div);\n      counter++;\n    }\n  }\n\n  faq(); // enter timeline \n\n  var tl = new TimelineMax();\n  tl.to(\"#things\", 1, {\n    opacity: 1\n  });\n  tl.to(\"#prices\", 1, {\n    opacity: 1\n  });\n  tl.to(\"#brands\", 1, {\n    opacity: 1\n  }); // init GSAP controller\n\n  var controller = new ScrollMagic.Controller({\n    globalSceneOptions: {\n      triggerHook: 'onLeave'\n    }\n  }); // pin email form to nav bar\n\n  var pin = new ScrollMagic.Scene({\n    triggerElement: '#test',\n    offset: -10\n  }).setPin('#test');\n  pin.setClassToggle('#test', 'nav-class');\n  pin.addTo(controller); // show Toothbrush\n\n  var showToothbrush = new ScrollMagic.Scene({\n    triggerElement: '#two'\n  });\n  showToothbrush.setClassToggle('#toothbrush', 'display');\n  showToothbrush.on(\"enter\", function () {\n    return changeBrand(\"toothbrush\");\n  });\n  showToothbrush.on(\"leave\", function () {\n    return changeBrand(\"shoe\");\n  });\n  showToothbrush.addTo(controller); // hide Shoe\n\n  var hideShoe = new ScrollMagic.Scene({\n    triggerElement: '#two'\n  });\n  hideShoe.setClassToggle('#shoe', 'hidden');\n  hideShoe.addTo(controller); // show Lipstick\n\n  var showLipstick = new ScrollMagic.Scene({\n    triggerElement: '#four'\n  });\n  showLipstick.setClassToggle('#lipstick', 'display');\n  showLipstick.on(\"enter\", function () {\n    return changeBrand(\"lipstick\");\n  });\n  showLipstick.on(\"leave\", function () {\n    return changeBrand(\"toothbrush\");\n  });\n  showLipstick.addTo(controller); // hideToothbrush\n\n  var hideToothBrush = new ScrollMagic.Scene({\n    triggerElement: '#four'\n  });\n  hideToothBrush.setClassToggle('#toothbrush', 'hidden');\n  hideToothBrush.addTo(controller); // section three animations\n\n  var enterY = new ScrollMagic.Scene({\n    triggerElement: '#two'\n  });\n  enterY.setClassToggle('');\n  enterY.addTo(controller);\n\n  function changeBrand(el) {\n    var price = document.getElementById('price-tag');\n    var factory = document.getElementById('factory');\n    var brand = document.getElementById('from-brand');\n\n    switch (el) {\n      case \"shoe\":\n        price.innerText = \"$80\";\n        factory.innerText = \"These shoes are made in the same factory as:\";\n        brand.innerText = \"Nike\";\n        break;\n\n      case \"toothbrush\":\n        price.innerText = \"$35\";\n        factory.innerText = \"This toothbrush is made in the same factory as:\";\n        brand.innerText = \"Quip\";\n        break;\n\n      case \"lipstick\":\n        price.innerText = \"$20\";\n        factory.innerText = \"This lipstick is made in the same factory as:\";\n        brand.innerText = \"La Mer\";\n        break;\n\n      default:\n        price.innerText = \"$80\";\n        factory.innerText = \"These shoes are made in the same factory as:\";\n        brand.innerText = \"Nike\";\n        break;\n    }\n  }\n});\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ })
+
+/******/ });
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function (e) {
 
-
     function faq() {
         let sect = document.getElementsByClassName('questions-container')[0]
-        console.log(data["faq"])
 
         let counter = 0
         while (counter < 4) {
@@ -27,55 +25,67 @@ document.addEventListener('DOMContentLoaded', function (e) {
     }
 
     faq()
-
-
-    // init GSAP controller
+    
+    // enter timeline 
+    var enterTimeline = new TimelineMax()
+    enterTimeline.to("#things", 1, { opacity: 1 })
+    enterTimeline.to("#prices", 1, { opacity: 1 })
+    enterTimeline.to("#brands", 1, { opacity: 1 })
 
     // init GSAP controller
     var controller = new ScrollMagic.Controller({
         globalSceneOptions: {
             triggerHook: 'onLeave'
         }
-    });
+    })
 
     // pin email form to nav bar
     var pin = new ScrollMagic.Scene({
         triggerElement: '#test',
-    }).setPin('#test');
-    pin.setClassToggle('#test', 'nav-class');
-    pin.addTo(controller);
+        offset: -10,
+    }).setPin('#test')
+    pin.setClassToggle('#test', 'nav-class')
+    pin.addTo(controller)
 
     // show Toothbrush
     var showToothbrush = new ScrollMagic.Scene({
         triggerElement: '#two',
     })
-    showToothbrush.setClassToggle('#toothbrush', 'display');
-    showToothbrush.on("enter", () => changeBrand("toothbrush"));
-    showToothbrush.on("leave", () => changeBrand("shoe"));
+    showToothbrush.setClassToggle('#toothbrush', 'display')
+    showToothbrush.on("enter", () => changeBrand("toothbrush"))
+    showToothbrush.on("leave", () => changeBrand("shoe"))
     showToothbrush.addTo(controller)
 
     // hide Shoe
     var hideShoe = new ScrollMagic.Scene({
         triggerElement: '#two',
     })
-    hideShoe.setClassToggle('#shoe', 'hidden');
+    hideShoe.setClassToggle('#shoe', 'hidden')
+    hideShoe.on("progress", () => enterYTween())    
     hideShoe.addTo(controller)
   
     // show Lipstick
     var showLipstick = new ScrollMagic.Scene({
         triggerElement: '#four',
     })
-    showLipstick.setClassToggle('#lipstick', 'display');
-    showLipstick.on("enter", () => changeBrand("lipstick"));
-    showLipstick.on("leave", () => changeBrand("toothbrush"));
+    showLipstick.setClassToggle('#lipstick', 'display')
+    showLipstick.on("enter", () => changeBrand("lipstick"))
+    showLipstick.on("leave", () => changeBrand("toothbrush"))
     showLipstick.addTo(controller)
 
     // hideToothbrush
     var hideToothBrush = new ScrollMagic.Scene({
         triggerElement: '#four',
     })
-    hideToothBrush.setClassToggle('#toothbrush', 'hidden');
+    hideToothBrush.setClassToggle('#toothbrush', 'hidden')
     hideToothBrush.addTo(controller)
+
+
+    function enterYTween() {
+        TweenMax.fromTo(".section-three-sub-head", 1, { opacity: 0, y: 50 }, { opacity: 1, y: 0, delay: 1.5 })
+        TweenMax.fromTo("#box-1", 1, { opacity: 0, y: 50 }, { opacity: 1, y: 0, delay: 2.5 })
+        TweenMax.fromTo("#box-2", 1, { opacity: 0, y: 50 }, { opacity: 1, y: 0, delay: 3.5 })
+    }
 
 
     function changeBrand(el) {
@@ -88,32 +98,24 @@ document.addEventListener('DOMContentLoaded', function (e) {
                 price.innerText = "$80"
                 factory.innerText = "These shoes are made in the same factory as:"
                 brand.innerText = "Nike"
-                break;
+                break
             case "toothbrush":
                 price.innerText = "$35"
                 factory.innerText = "This toothbrush is made in the same factory as:"
                 brand.innerText = "Quip"
-                break;
+                break
             case "lipstick":
                 price.innerText = "$20"
                 factory.innerText = "This lipstick is made in the same factory as:"
                 brand.innerText = "La Mer"
-                break;
+                break
             default:
                 price.innerText = "$80"
                 factory.innerText = "These shoes are made in the same factory as:"
                 brand.innerText = "Nike"
-                break;
+                break
         }
     }
-
-
-
-    //enter timeline 
-    // var tl = new TimelineMax();
-    // tl.to("#things", 1.5, { opacity: 1 });
-    // tl.to("#prices", 1.5, { opacity: 1 });
-    // tl.to("#brands", 1.5, { opacity: 1 });
 
 
    
